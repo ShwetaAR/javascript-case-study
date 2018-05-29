dd8d8881bb47c1da263193c2389e8e62437438da
Shweta Baberia <Shweta.baberia@Yash.com>
2018-05-29 12:38:59 +0530

    coke song commit

--- coke-song/coke-song.spec.js
+++ coke-song/coke-song.spec.js
@@ -28,9 +28,14 @@ describe('Song', function () {
     expect(song.sing(8, 6)).toEqual(expected);
   });
 
-  fit('sings the rest of the verses', function () {
+  it('sings the rest of the verses', function () {
     var expected = '3 cans of coke in the box, 3 cans of coke.\nTake one down and pass it around, 2 cans of coke in the box.\n\n2 cans of coke in the box, 2 cans of coke.\nTake one down and pass it around, 1 can of coke in the box.\n\n1 can of coke in the box, 1 can of coke.\nTake it down and pass it around, no more cans of coke in the box.\n\nNo more cans of coke in the box, no more cans of coke.\nGo to the store and buy some more, 99 cans of coke in the box.\n';
     expect(song.sing(3)).toEqual(expected);
+  });
+
+  it('full song', function () {
+    var expected = 'full song';
+    expect(song.sing()).toEqual(expected);
   });
 });
 
